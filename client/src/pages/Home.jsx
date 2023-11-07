72221ccc8adf16ced117cee47ac548609d1de3b7
AnkurYadav65439 <ay0695617@gmail.com>
2023-11-07 20:26:06 +0530

    complete home and about pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='px-4 py-12 max-w-2xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>Welcome to my Auth App!</h1>
+      <p className='mb-4 text-slate-700'>This is a full-stack web application built with the NERN (MongoDB, Express, React, Node.js) stack. It includes authentication features that allow users to sign up, log in, log out, and provides access to protected routes only for authenticated users.</p>
+      <p className='mb-4 text-slate-700'>This is a full-stack web application built with the NERN (MongoDB, Express, React, Node.js) stack. It includes authentication features that allow users to sign up, log in, log out, and provides access to protected routes only for authenticated users.</p>
+    </div>
   )
 }
