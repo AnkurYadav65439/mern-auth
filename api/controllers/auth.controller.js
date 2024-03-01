cd5a58c1106d40a0c6209b7e23bd64a2f1f8e637
AnkurYadav65439 <ay0695617@gmail.com>
2024-03-01 12:43:04 +0530

    final chsnges

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -6,6 +6,7 @@ import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
+    //genSalt can also be used here before hash
     const hashPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashPassword });
     try {
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //added by me
+  //added by me for CORS ERROR
   server: {
     proxy: {
       '/api': {
